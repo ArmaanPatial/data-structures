7e16791870006454e6158c72599882002f159900
Armaan Patial <armaanpatial47@gmail.com>
2024-01-18 00:07:17 +0530

    commit

--- reverseNum.java
+++ reverseNum.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class reverseNum {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n>0) {
+            int rem = n % 10;
+            ans = (ans*10) + rem;
+            n = n / 10;
+        } 
+        System.out.println("reverse of the number is : "+num);
+    }
 }
