445713ae61f057d12292181bbe653e4be67f144d
Armaan Patial <armaanpatial47@gmail.com>
2024-01-18 02:33:46 +0530

    commit 5 patterns

--- ads.java
+++ ads.java
@@ -1,8 +1,56 @@
 public class ads {
     public static void main(String[] args) {
-        
-        for (int i = 5; i > 0; i--) {
-            System.out.print("*"+" ");
+        pattern5(5);
+    }
+
+    static void pattern1(int n){
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
+
+    static void pattern4(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
     
